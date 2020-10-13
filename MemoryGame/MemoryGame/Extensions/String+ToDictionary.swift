6011b293e098eec4281ba7160bcc7a0d18f814c1
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-10-13 11:50:27 -0300

    create extension to Array and create function toDictionary

---
+++ MemoryGame/MemoryGame/Extensions/String+ToDictionary.swift
@@ -0,0 +1,24 @@
+//
+//  String+ToDictionary.swift
+//  MemoryGame
+//
+//  Created by Leonardo dos Santos Marcos on 13/10/20.
+//
+
+import Foundation
+
+extension Array {
+    func toDictionary() -> Dictionary<Int, String> {
+        if let array = self as? [String] {
+            var dictionary : [Int : String] = [:]
+           
+            for i in 0...array.count - 1 {
+                dictionary[i + 1] = array[i]
+            }
+            
+            return dictionary
+        }
+        
+        return [:]
+    }
+}
