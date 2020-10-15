aa53821a11eb95f6ed4d9750356516b5d693d847
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-10-15 16:25:27 -0300

    modify images

---
+++ MemoryGame/MemoryGame/Components/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  MemoryGame
+//
+//  Created by Leonardo dos Santos Marcos on 14/10/20.
+//
+
+import UIKit
+
+class Card: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
