266cbf262416660fef55baa0821ea845743db9bf
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-10-15 16:26:39 -0300

    create card

--- MemoryGame/MemoryGame/Components/Card.swift
+++ MemoryGame/MemoryGame/Components/Card.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class Card: UICollectionViewCell {
-
+    static var id: String = "Card"
+    @IBOutlet var imageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+    }
+    
+    public func configure(with image: UIImage) {
+        imageView.image = image
+        
+//        layer.shadowColor = UIColor.black.cgColor
+//        layer.shadowOpacity = 1
+//        layer.shadowOffset = .zero
+//        layer.shadowRadius = 10
+        
+        let cornerRadius: CGFloat = 8
+        layer.cornerRadius = cornerRadius
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 0.5
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: id, bundle: nil)
     }
 
 }
